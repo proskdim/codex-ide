8c88dc07f9449c069f7981273315c6b009fd5d5c
proskurekov <proskurekovdim@gmail.com>
2026-01-09 19:45:41 +0700

    update editor

--- src/app/components/codex-editor/codex-editor/codex-editor.ts
+++ src/app/components/codex-editor/codex-editor/codex-editor.ts
@@ -1,14 +1,20 @@
-import { Component, output, signal } from '@angular/core';
+import { Component, Injectable, output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
 
+@Injectable({ providedIn: 'root' })
 @Component({
   selector: 'codex-editor',
-  imports: [],
+  imports: [MonacoEditorModule, FormsModule],
   templateUrl: './codex-editor.html',
   styleUrl: './codex-editor.css',
 })
 
 export class CodexEditor {
   showEditor = output();
+
+  editorOptions = {language: 'typescript', tabSize: 2, minimap: {enabled: false}};
+  code: string= 'function x() {\nconsole.log("Hello world!");\n}';
 
   onCloseEditor() {
     this.showEditor.emit();
