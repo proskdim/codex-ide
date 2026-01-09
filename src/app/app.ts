62dcb25a7a10d5d228cee30ef5b143165f3c33bb
proskurekov <proskurekovdim@gmail.com>
2026-01-09 15:40:24 +0700

    init

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CodexEditor } from './components/codex-editor/codex-editor/codex-editor';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, CodexEditor],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('codex-ide');
+
+  showCodexEditor = signal(true);
+
+  onShowCodexEditor() {
+    this.showCodexEditor.set(!this.showCodexEditor());
+  }
 }
---
+++ src/app/components/codex-editor/codex-editor/codex-editor.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'codex-editor',
+  imports: [],
+  templateUrl: './codex-editor.html',
+  styleUrl: './codex-editor.css',
+})
+
+export class CodexEditor {
+
+}
