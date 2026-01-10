683a3fc5c0dfc200528962310879a73806f3c6a9
proskurekov <proskurekovdim@gmail.com>
2026-01-10 18:59:41 +0700

    add @tailwindcss/typography plugin and enhance editor description

--- src/app/components/codex-editor/codex-editor/codex-editor.ts
+++ src/app/components/codex-editor/codex-editor/codex-editor.ts
@@ -31,8 +31,10 @@ export class CodexEditor {
   /** Emits when the editor should be closed. */
   readonly showEditor = output<void>();
 
-  /** Markdown description content. */
-  readonly description = input<string>('# Description\n\nThis is a description of the code example.');
+  /** Markdown big description content. */
+  readonly description = input<string>(
+    `# Description\nThis is a description of the code example. \n\n## Subtitle\nThis is a subtitle of the description. \n\n### Subsubtitle\nThis is a subsubtitle of the description. \n\n- List item 1\n- List item 2\n- List item 3`
+  );
 
   /** Code content in the editor. */
   readonly code = model<string>('function x() {\n\tconsole.log("Hello world!");\n}');
